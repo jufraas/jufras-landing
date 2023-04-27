c44038ef2fbad89e8d3a0a8edf104e9b5f7a4ae7
jufraas <jufracamachoposso@gmail.com>
2023-04-27 13:25:10 -0500

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/*/.{js,ts,jsx,tsx}',
   ],
+    darkMode: false,
   theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'purple2': '#B17CF4',
+      'orange': '#ff7849',
+      'green-light': '#13ce66',
+      'green': '#69B865',
+      'yellow': '#ffc82c',
+      'gray-blue': '#273444',
+      gray: {
+        100: '#EAEAEA',
+        200: '#C7C7C7',
+        300: '#9D9D9D',
+        400: '#5C5C5C',
+        500: '#444444',
+        600: '#373737',
+        700: '#2E2E2E',
+        800: '#232323'
+      },
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
